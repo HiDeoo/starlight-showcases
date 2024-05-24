ba9be8f051100f081d2f2f99d1f63c741157a388
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-05-24 18:22:37 +0200

    feat: add `<ShowcaseImage>` component

--- packages/starlight-showcases/index.ts
+++ packages/starlight-showcases/index.ts
@@ -1,2 +1,3 @@
 export { default as ShowcaseText, type ShowcaseTextProps } from './components/ShowcaseText.astro'
 export { default as ShowcaseYoutube, type ShowcaseYoutubeProps } from './components/ShowcaseYoutube.astro'
+export { default as ShowcaseImage, type ShowcaseImageProps } from './components/ShowcaseImage.astro'
