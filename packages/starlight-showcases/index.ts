18705221a9205c582d2e06f81bafea967e489f6a
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-05-24 16:36:56 +0200

    feat: add `<ShowcaseYoutube>` component

--- packages/starlight-showcases/index.ts
+++ packages/starlight-showcases/index.ts
@@ -1,1 +1,2 @@
 export { default as ShowcaseLinks, type ShowcaseLinksProps } from './components/ShowcaseLinks.astro'
+export { default as ShowcaseYoutube, type ShowcaseYoutubeProps } from './components/ShowcaseYoutube.astro'
