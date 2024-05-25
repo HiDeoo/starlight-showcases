da577172f7be197f72015631e3954b67019019b0
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-05-25 10:57:16 +0200

    feat: add `<ShowcaseQuote>` component

--- packages/starlight-showcases/index.ts
+++ packages/starlight-showcases/index.ts
@@ -1,4 +1,5 @@
 export { default as ShowcaseText, type ShowcaseTextProps } from './components/ShowcaseText.astro'
 export { default as ShowcaseImage, type ShowcaseImageProps } from './components/ShowcaseImage.astro'
+export { default as ShowcaseQuote, type ShowcaseQuoteProps } from './components/ShowcaseQuote.astro'
 export { default as ShowcaseYoutube, type ShowcaseYoutubeProps } from './components/ShowcaseYoutube.astro'
 export { default as ShowcaseTwitter, type ShowcaseTwitterProps } from './components/ShowcaseTwitter.astro'
