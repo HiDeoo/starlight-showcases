5097612c58a613d44d3dd15683f3e75c2d2ffb0b
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-12-15 11:29:20 +0100

    feat: add support for astro 5

---
+++ packages/starlight-showcases/eslint.config.mjs
@@ -0,0 +1,13 @@
+import hideoo from '@hideoo/eslint-config'
+
+export default hideoo({
+  ignores: ['eslint.config.mjs'],
+  languageOptions: {
+    parserOptions: {
+      project: ['../../tsconfig.json'],
+    },
+  },
+  rules: {
+    'unicorn/filename-case': 'off',
+  },
+})
